d917c0f3ecc2fd46b3e2f3f93f74e55fe81c9dc6
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-03-10 19:26:35 +0000

    switching to a fixed UI sidebar, saved the pop-out one as a prefab just in case. re-sized and repositioned the browser window to match the added sidebar

--- Assets/Scripts/WindowManager.cs
+++ Assets/Scripts/WindowManager.cs
@@ -9,7 +9,7 @@ public class WindowManager : MonoBehaviour
     //function to open a game window
     public void OpenBrowserWindow() {
         //obj.SetActive(true);
-        Instantiate(browserWindow,windows.transform.position,windows.transform.rotation,windows.transform);
+        Instantiate(browserWindow,new Vector2(windows.transform.position.x - 220, windows.transform.position.y + 75),windows.transform.rotation,windows.transform);
     }
 
     public void OpenWindow(GameObject window) {
