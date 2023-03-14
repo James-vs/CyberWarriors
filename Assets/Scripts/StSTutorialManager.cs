3f463f7f8978eb015477bdfc822629b0ae38716e
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-03-14 17:04:14 +0000

    Completed basic UI functionality for the tutorial

--- Assets/Scripts/StSTutorialManager.cs
+++ Assets/Scripts/StSTutorialManager.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class StSTutorialManager : MonoBehaviour
 {
@@ -12,11 +12,28 @@ public class StSTutorialManager : MonoBehaviour
     [SerializeField] private bool match1_2 = false;
     [SerializeField] private bool match2_1 = false;
     [SerializeField] private bool match2_2 = false;
+    [SerializeField] private GameObject button1_1;
+    [SerializeField] private GameObject button1_2;
+    [SerializeField] private GameObject button2_1;
+    [SerializeField] private GameObject button2_2;
+    private Color btn1_1Original;
+    private Color btn1_2Original;
+    private Color btn2_1Original;
+    private Color btn2_2Original;
+
+    private int matches = 0;
+    [SerializeField] private TextMeshProUGUI matchesUI;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("StSTutorialManager running");
+        btn1_1Original = button1_1.GetComponent<Image>().color;
+        btn1_2Original = button1_2.GetComponent<Image>().color;
+        btn2_1Original = button2_1.GetComponent<Image>().color;
+        btn2_2Original = button2_2.GetComponent<Image>().color;
     }
 
     // Update is called once per frame
@@ -28,7 +45,8 @@ public class StSTutorialManager : MonoBehaviour
                 success.SetActive(true);
                 gameOver = true;
             }
-        } 
+        }
+        matchesUI.text = matches.ToString();
     }
 
     public bool CheckComplete() {
@@ -50,10 +68,15 @@ public class StSTutorialManager : MonoBehaviour
 
     public bool Match1Found() {
         if (match1_1 && match1_2) {
-            if (!m1First) Debug.Log("First pair matched");
+            if (!m1First) {
+                Debug.Log("First pair matched");
+                matches += 1;
+            }
             m1First = true;
             match1_1 = false;
             match1_2 = false;
+            button1_1.GetComponent<Image>().color = Color.green;
+            button1_2.GetComponent<Image>().color = Color.green;
             return true;
         } else {
             return false;
@@ -63,10 +86,15 @@ public class StSTutorialManager : MonoBehaviour
 
     public bool Match2Found() {
         if (match2_1 && match2_2) {
-            if (!m2First) Debug.Log("Second pair matched");
+            if (!m2First) {
+                Debug.Log("Second pair matched");
+                matches += 1;
+            }
             //match?_? added
             match2_1 = false;
             match2_2 = false;
+            button2_1.GetComponent<Image>().color = Color.green;
+            button2_2.GetComponent<Image>().color = Color.green;
             m2First = true;
             return true;
         } else {
@@ -79,8 +107,10 @@ public class StSTutorialManager : MonoBehaviour
             if (!match2_1 && !match1_1) {
                 Debug.Log("Button 1_1 selected");
                 match1_1 = true;
+                button1_1.GetComponent<Image>().color = Color.yellow;
             } else {
                 match1_1 = false;
+                button1_1.GetComponent<Image>().color = btn1_1Original;
             }
         }
     }
@@ -90,7 +120,9 @@ public class StSTutorialManager : MonoBehaviour
             if (!match2_2 && !match1_2) {
                 Debug.Log("Button 1_2 selected");
                 match1_2 = true;
+                button1_2.GetComponent<Image>().color = Color.yellow;
             } else {
+                button1_2.GetComponent<Image>().color = btn1_2Original;
                 match1_2 = false;
             }
         }
@@ -101,8 +133,10 @@ public class StSTutorialManager : MonoBehaviour
             if (!match1_1 && !match2_1) {
                 Debug.Log("Button 2_1 selected");
                 match2_1 = true;
+                button2_1.GetComponent<Image>().color = Color.yellow;
             } else {
                 match2_1 = false;
+                button2_1.GetComponent<Image>().color = btn2_1Original;
             }
         }
     }
@@ -112,8 +146,10 @@ public class StSTutorialManager : MonoBehaviour
             if (!match1_2 && !match2_2) {
                 Debug.Log("Button 2_2 selected");
                 match2_2 = true;
+                button2_2.GetComponent<Image>().color = Color.yellow;
             } else {
                 match2_2 = false;
+                button2_2.GetComponent<Image>().color = btn2_2Original;
             }
         }
     }
