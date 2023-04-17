197743e940ed05d1d6386b9e391a6506af2c29e0
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-04-17 16:58:26 +0100

    created cookies for second website, started creating the elements to match (incomplete)

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -4,17 +4,24 @@ using TMPro;
 
 public class SettingsManager : MonoBehaviour
 {
+    [SerializeField] protected GameObject background;
+    protected Color backgroundOriginal;
+    protected Color incBackground;
     [SerializeField] protected Text dNTText;
     [SerializeField] protected Text incText;
     [SerializeField] protected Text pSText;
-
+    [SerializeField] protected string level;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        backgroundOriginal = background.GetComponent<Image>().color;
+        incBackground = new Color32(72,82,113,255);
+        PlayerPrefs.SetInt(level+"DNT",0);
+        PlayerPrefs.SetInt(level+"Inc",0);
+        PlayerPrefs.SetInt(level+"PS",0);
     }
 
     // Update is called once per frame
@@ -28,24 +35,32 @@ public class SettingsManager : MonoBehaviour
     public void DNTToggle(bool enabled) {
         if (enabled) {
             dNTText.text = "Enabled";
+            PlayerPrefs.SetInt(level+"DNT",1);
         } else {
             dNTText.text = "Disabled";
+            PlayerPrefs.SetInt(level+"DNT",0);
         }
     }
 
     public void IncToggle(bool enabled) {
         if (enabled) {
             incText.text = "Enabled";
+            PlayerPrefs.SetInt(level+"Inc",1);
+            background.GetComponent<Image>().color = incBackground;
         } else {
             incText.text = "Disabled";
+            PlayerPrefs.SetInt(level+"Inc",0);
+            background.GetComponent<Image>().color = backgroundOriginal;
         }
     }
 
     public void PSToggle(bool enabled) {
         if (enabled) {
             pSText.text = "Enabled";
+            PlayerPrefs.SetInt(level+"PS",1);
         } else {
             pSText.text = "Disabled";
+            PlayerPrefs.SetInt(level+"PS",0);
         }
     }
 }
