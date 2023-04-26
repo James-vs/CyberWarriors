2a993f4227bc8dbeb8c36691f15ae20526829653
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-04-26 13:15:04 +0100

    created malware info select page plus navigation functionality

---
+++ Assets/InfoPageManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPageManager : MonoBehaviour
+{
+    [SerializeField] protected GameObject malwareSelectPage;
+    [SerializeField] protected GameObject virusInfoPage;
+    //[SerializeField] protected GameObject trojanInfoPage;
+    //[SerializeField] protected GameObject ransomInfoPage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    // function to take player back to info page select 
+    public void Back() {
+        malwareSelectPage.SetActive(true);
+        virusInfoPage.SetActive(false);
+        //trojanInfoPage.SetActive(false);
+        //ransomInfoPage.SetActive(false);
+    }
+
+
+
+
+    // functions to open associated info pages
+    public void OpenVirusInfo() {
+        malwareSelectPage.SetActive(false);
+        virusInfoPage.SetActive(true);
+    }
+    public void OpenTrojanInfo() {
+        malwareSelectPage.SetActive(false);
+        //trojanInfoPage.SetActive(true);
+    }
+    public void OpenRansomInfo() {
+        malwareSelectPage.SetActive(false);
+        //ransomInfoPage.SetActive(true);
+    }
+
+
+
+
+    // function to return to the game from the info pages
+    public void BackToGame() {
+        this.transform.gameObject.SetActive(false);
+    }
+}
