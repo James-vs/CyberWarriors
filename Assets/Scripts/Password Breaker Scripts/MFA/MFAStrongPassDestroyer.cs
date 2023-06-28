8ec885899c4b3cc716593011f586519eaee9e32a
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-06-28 16:08:53 +0100

    patched the strong 2fa brick collision bug

--- Assets/Scripts/Password Breaker Scripts/MFA/MFAStrongPassDestroyer.cs
+++ Assets/Scripts/Password Breaker Scripts/MFA/MFAStrongPassDestroyer.cs
@@ -15,6 +15,21 @@ public class MFAStrongPassDestroyer : MonoBehaviour
 
     //method to Destroy the brick if a ball collides with it twice
     public void OnCollisionEnter2D(Collision2D other) {
+        /*if (!other.gameObject.CompareTag("Ball")) return;
+        SpriteRenderer brick = this.GetComponent<SpriteRenderer>();
+        if (collisionCount == 2) {
+            collisionCount -= 1f;
+            brick.color = new Color32(40, 180, 0, 255);
+        } else if (collisionCount == 1){
+            collisionCount -= 1f;
+            brick.color = new Color32(40, 180, 0, 100);
+        } else {
+            Destroy(gameObject);
+            Instantiate(go, transform.position, transform.rotation);
+        }*/
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
         if (!other.gameObject.CompareTag("Ball")) return;
         SpriteRenderer brick = this.GetComponent<SpriteRenderer>();
         if (collisionCount == 2) {
