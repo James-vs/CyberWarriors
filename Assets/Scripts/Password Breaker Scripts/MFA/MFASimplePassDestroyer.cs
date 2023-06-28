431dbda242d6d319ca8757d383250908642079b1
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-06-28 15:22:33 +0100

    fixed the 2FA collision bug for simple 2fa blocks. also rearranged the prefabs folder so i can easily distinguish between different games and their prefabs

--- Assets/Scripts/Password Breaker Scripts/MFA/MFASimplePassDestroyer.cs
+++ Assets/Scripts/Password Breaker Scripts/MFA/MFASimplePassDestroyer.cs
@@ -14,6 +14,11 @@ public class MFASimplePassDestroyer : MonoBehaviour
     //method to Destroy the brick if a ball collides with it
     public void OnCollisionEnter2D(Collision2D other) {
         if (!other.gameObject.CompareTag("Ball")) return;
+        // possibly dont need this function
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (!other.gameObject.CompareTag("Ball")) return;
         Destroy(gameObject);
         Instantiate(go, transform.position, transform.rotation);
     }
