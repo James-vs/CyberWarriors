1b3dcee969756248bc312c1d1dd7b49410da7eca
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-07-27 13:33:38 +0100

    fixed the tab select bug for real and moved original G1Level1 back from old version to cyberwarriors

--- Assets/Scripts/Secureme Scripts/BrowserManager.cs
+++ Assets/Scripts/Secureme Scripts/BrowserManager.cs
@@ -11,18 +11,22 @@ public class BrowserManager : MonoBehaviour
 
     //function to switch to tab 2
     public virtual void SwitchToTab2() {
-        tab1.SetActive(true);
-        tab1Button.GetComponent<Button>().interactable = false;
-        tab2.SetActive(false);
-        tab2Button.GetComponent<Button>().interactable = true;
+        tab1.SetActive(false);
+        //tab1Button.GetComponent<Button>().interactable = true;
+        tab1Button.GetComponent<Image>().color = new Color32(200,200,200,128);
+        tab2.SetActive(true);
+        //tab2Button.GetComponent<Button>().interactable = false;
+        tab2Button.GetComponent<Image>().color = new Color32(255,255,255,255);
     }
 
     //function to switch to tab1
     public virtual void SwitchToTab1(){
-        tab2.SetActive(true);
-        tab2Button.GetComponent<Button>().interactable = false;
-        tab1.SetActive(false);
-        tab1Button.GetComponent<Button>().interactable = true;
+        tab2.SetActive(false);
+        //tab2Button.GetComponent<Button>().interactable = true;
+        tab2Button.GetComponent<Image>().color = new Color32(200,200,200,128);
+        tab1.SetActive(true);
+        //tab1Button.GetComponent<Button>().interactable = false;
+        tab1Button.GetComponent<Image>().color = new Color32(255,255,255,255);
         
     }
 
@@ -30,6 +34,5 @@ public class BrowserManager : MonoBehaviour
     public void Close() {
         Destroy(gameObject);
     }
-
 
 }
