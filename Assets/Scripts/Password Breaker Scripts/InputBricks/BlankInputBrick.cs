c7d93d94bfe48a0ff35a5d21e8f3a06dffb61988
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-08-13 14:29:10 +0100

    created a basic, functional input brick in test scene 1

--- Assets/Scripts/Password Breaker Scripts/InputBricks/BlankInputBrick.cs
+++ Assets/Scripts/Password Breaker Scripts/InputBricks/BlankInputBrick.cs
@@ -1,10 +1,14 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class BlankInputBrick : MonoBehaviour
 {
     protected GameObject InputWindow;
     protected BoxCollider2D boxCollider;
+    protected string inputPassword;
+    protected bool hasNewPassword = false;
+    [SerializeField] protected TextMeshPro brickText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,31 @@ public class BlankInputBrick : MonoBehaviour
 
     private void OnMouseDown()
     {
-        InputWindow.SetActive(true);
+        if (!hasNewPassword)
+        {
+            InputWindow.SetActive(true);
+            Time.timeScale = 0f;
+            for (int i = 0; i < InputWindow.transform.childCount; i++) 
+            { 
+                InputWindow.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void CloseInputWindow()
+    {
+        Time.timeScale = 1.0f;
+        for (int i = 0; i < InputWindow.transform.childCount; i++)
+        {
+            InputWindow.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        brickText.text = inputPassword;
+        hasNewPassword = true;
+        Debug.Log("Inputted text: " + inputPassword);
+    }
+
+    public void GetInputString(string newPassword)
+    {
+        inputPassword = newPassword;
     }
 }
