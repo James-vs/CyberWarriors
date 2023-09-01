c2eb727d0a34374ad1b3da20e299013fe02c3a77
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-09-01 19:23:48 +0100

    attemted to recieve user data from website but code isn't working. need to figure out why...

--- Assets/SessionController.cs
+++ Assets/SessionController.cs
@@ -5,6 +5,8 @@
 
 using UnityEngine;
 using System.Runtime.InteropServices;
+using UnityEngine.Networking;
+using System.Collections;
 
 // The GameObject that this is attached to MUST be called "SessionController".
 public class SessionController : MonoBehaviour
@@ -12,6 +14,9 @@ public class SessionController : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void RequestWebSession();
     [SerializeField] private string pBSessionID = "";
+    [SerializeField] private string pBPlayPrefSessionKey = "PBSessionKey";
+    [SerializeField] private string pBSecretKey = "a19b9b6866c5422bd7a1753da27fd8afc8f5d139";
+    [SerializeField] private string pBUrl = "https://cybersec-web-app-frontend-production.up.railway.app/";
 
     // Call THIS function at the start of your game to request that the frontend sends a sessionID
     public void RequestSession()
@@ -19,6 +24,8 @@ public class SessionController : MonoBehaviour
 #if UNITY_WEBGL == true && UNITY_EDITOR == false
     	RequestWebSession();
 #endif
+        // below code used for testing purposes, should be deleted later
+        StartCoroutine(MakeRequests(RequestType.USERDATA));
     }
 
     // This method is called when a sessionID is sent by the frontend
@@ -27,8 +34,77 @@ public class SessionController : MonoBehaviour
     {
         // You can do what you want with the sessionID here. Ideally you should store it somewhere so that you can use it to make calls to the API later.
         pBSessionID = sessionID;
-
+        PlayerPrefs.SetString(pBPlayPrefSessionKey, pBSessionID);
         Debug.Log($"Session received: {pBSessionID}");
     }
+
+    private UnityWebRequest GetUserInformation() 
+    {
+        var request = new UnityWebRequest(pBUrl + "api/user", "GET");
+
+        request.downloadHandler = new DownloadHandlerBuffer();
+
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("secret", pBSecretKey);
+        request.SetRequestHeader("session", pBSessionID);
+
+        return request;
+    }
+
+
+    private IEnumerator MakeRequests(RequestType type)
+    {
+        // NOTE: you cannot convert JsonUtility.FromJson... data to string using .ToString()
+        string deserialisedData = null;
+        switch (type)
+        {
+            case RequestType.USERDATA:
+                // this chunk doesn't work!...
+
+                Debug.Log("Attempting request for user data");
+                var userDataRequest = GetUserInformation();
+                yield return userDataRequest.SendWebRequest();
+                deserialisedData = JsonUtility.FromJson<string>(userDataRequest.downloadHandler.text);
+                Debug.Log("data recieved: "+JsonUtility.FromJson<string>(userDataRequest.downloadHandler.text));
+                break;
+            
+            default:
+                Debug.Log("Incorrect RequestType value given to MakeRequests() function");
+                break;  
+        }
+
+        if (deserialisedData != null) 
+        {
+            Debug.Log("User Data Recieved");
+            /*Debug.Log("id: " + deserialisedData.id +
+                "\nusername: " + deserialisedData.username +
+                "\nisDeveloper: " + deserialisedData.isDeveloper +
+                "\nscore: " + deserialisedData.score);*/
+        }
+        else
+        {
+            Debug.Log("Bombaclart");
+        }
+    }
+
+    public enum RequestType
+    {
+        USERDATA = 0,
+        LEADERBOARD = 1,
+        SCOREPOST = 2
+    }
+
+    /// <summary>
+    /// class for recieving data from user data get request
+    /// </summary>
+    public class UserData
+    {
+        public string id;
+        public string username;
+        public bool isDeveloper;
+        public int score;
+    }
+
+
 }
 
