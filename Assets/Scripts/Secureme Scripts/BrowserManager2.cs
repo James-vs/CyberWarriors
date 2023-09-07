1a2c6b9dc3b9a24875b739806270228b46fc1c10
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-09-07 19:50:49 +0100

    level 3 updated - side panel and top bar now done

--- Assets/Scripts/Secureme Scripts/BrowserManager2.cs
+++ Assets/Scripts/Secureme Scripts/BrowserManager2.cs
@@ -7,32 +7,47 @@ public class BrowserManager2 : BrowserManager
     [SerializeField] protected Button settingsBtn;
 
 
+    private void Start()
+    {
+        SwitchToSettings();
+    }
+
     //function to switch to tab 2
     public override void SwitchToTab2() {
         tab1.SetActive(false);
-        tab1Button.GetComponent<Button>().interactable = true;
+        //tab1Button.GetComponent<Button>().interactable = true;
+        tab1Button.GetComponent<Image>().color = new Color32(200, 200, 200, 128);
         tab2.SetActive(true);
-        tab2Button.GetComponent<Button>().interactable = false;
+        //tab2Button.GetComponent<Button>().interactable = false;
+        tab2Button.GetComponent<Image>().color = new Color32(255, 255, 255, 255);        
         settings.SetActive(false);
-        settingsBtn.GetComponent<Button>().interactable = true;
+        //settingsBtn.GetComponent<Button>().interactable = true;
+        settingsBtn.GetComponent<Image>().color = new Color32(200, 200, 200, 128);
     }
 
-    //function to switch to tab1
+    //function to switch to tab 1
     public override void SwitchToTab1(){
-        tab2.SetActive(false);
-        tab2Button.GetComponent<Button>().interactable = true;
-        tab1.SetActive(true);
-        tab1Button.GetComponent<Button>().interactable = false;
         settings.SetActive(false);
-        settingsBtn.GetComponent<Button>().interactable = true;
+        //settingsBtn.GetComponent<Button>().interactable = true;
+        settingsBtn.GetComponent<Image>().color = new Color32(200, 200, 200, 128);
+        tab2.SetActive(false);
+        //tab2Button.GetComponent<Button>().interactable = true;
+        tab2Button.GetComponent<Image>().color = new Color32(200, 200, 200, 128);
+        tab1.SetActive(true);
+        //tab1Button.GetComponent<Button>().interactable = false;
+        tab1Button.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
     }
 
     public virtual void SwitchToSettings() {
         tab2.SetActive(false);
-        tab2Button.GetComponent<Button>().interactable = true;
+        //tab2Button.GetComponent<Button>().interactable = true;
+        tab2Button.GetComponent<Image>().color = new Color32(200, 200, 200, 128);
         tab1.SetActive(false);
-        tab1Button.GetComponent<Button>().interactable = true;
+        //tab1Button.GetComponent<Button>().interactable = true;
+        tab1Button.GetComponent<Image>().color = new Color32(200, 200, 200, 128);
         settings.SetActive(true);
-        settingsBtn.GetComponent<Button>().interactable = false;
+        //settingsBtn.GetComponent<Button>().interactable = false;
+        settingsBtn.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
     }
 }
