d63cdae1b7b4f167a4c23d33597400a61698acee
jcvs1g20 <jcvs1g20@soton.ac.uk>
2023-09-19 16:13:09 +0100

    fixed the cookies selection panel - can now select any cookie without having to deselect first

--- Assets/Scripts/Secureme Scripts/Level Managers/CookieManager.cs
+++ Assets/Scripts/Secureme Scripts/Level Managers/CookieManager.cs
@@ -9,6 +9,8 @@ public class CookieManager : MonoBehaviour
     [SerializeField] private GameObject strictlyNec;
     [SerializeField] private Button doneBtn;
     [SerializeField] private string playerPrefsVarName;
+    //[SerializeField] private Color normal;
+    //[SerializeField] private Color notSelected = new Color(125, 125, 125, 128);
     private int option;
 
     // Start is called before the first frame update
@@ -16,6 +18,7 @@ public class CookieManager : MonoBehaviour
     {
         doneBtn.gameObject.SetActive(false);
         acceptAll.GetComponent<Toggle>().Select();
+        //normal = acceptAll.GetComponent<Toggle>().colors.normalColor;
         OnToggleSelect3(true);
 
     }
@@ -26,37 +29,46 @@ public class CookieManager : MonoBehaviour
     /// <param name="selected">bool value for de/select</param>
     public void OnToggleSelect1(bool selected) {
         if (selected == true) {
-            acceptAll.GetComponent<Toggle>().interactable = false;
-            strictlyNec.GetComponent<Toggle>().interactable = false;
-            doneBtn.gameObject.SetActive(true);
-            option = 1;
+            Activate(1);
         } else {
-            acceptAll.GetComponent<Toggle>().interactable = true;
-            strictlyNec.GetComponent<Toggle>().interactable = true;
+            //acceptAll.GetComponent<Toggle>().interactable = true;
+            /*var aColors = acceptAll.GetComponent<Toggle>().colors;
+            aColors.normalColor = normal;
+            acceptAll.GetComponent<Toggle>().colors = aColors;
+            //strictlyNec.GetComponent<Toggle>().interactable = true;
+            var sColors = strictlyNec.GetComponent<Toggle>().colors;
+            sColors.normalColor = normal;
+            strictlyNec.GetComponent<Toggle>().colors = sColors;*/
             doneBtn.gameObject.SetActive(false);
         }
     }
     public void OnToggleSelect2(bool selected) {
         if (selected == true) {
-            acceptAll.GetComponent<Toggle>().interactable = false;
-            rejectAll.GetComponent<Toggle>().interactable = false;
-            doneBtn.gameObject.SetActive(true);
-            option = 2;
+            Activate(2);
         } else {
-            acceptAll.GetComponent<Toggle>().interactable = true;
-            rejectAll.GetComponent<Toggle>().interactable = true;
+            //acceptAll.GetComponent<Toggle>().interactable = true;
+            /*var aColors = acceptAll.GetComponent<Toggle>().colors;
+            aColors.normalColor = normal;
+            acceptAll.GetComponent<Toggle>().colors = aColors;
+            //rejectAll.GetComponent<Toggle>().interactable = true;
+            var rColors = rejectAll.GetComponent<Toggle>().colors;
+            rColors.normalColor = normal;
+            rejectAll.GetComponent<Toggle>().colors = rColors;*/
             doneBtn.gameObject.SetActive(false);
         }
     }
     public void OnToggleSelect3(bool selected) {
         if (selected == true) {
-            strictlyNec.GetComponent<Toggle>().interactable = false;
-            rejectAll.GetComponent<Toggle>().interactable = false;
-            doneBtn.gameObject.SetActive(true);
-            option = 0;
+            Activate(3);
         } else {
-            strictlyNec.GetComponent<Toggle>().interactable = true;
-            rejectAll.GetComponent<Toggle>().interactable = true;
+            //strictlyNec.GetComponent<Toggle>().interactable = true;
+            /*var sColors = strictlyNec.GetComponent<Toggle>().colors;
+            sColors.normalColor = normal;
+            strictlyNec.GetComponent<Toggle>().colors = sColors;*/
+            //rejectAll.GetComponent<Toggle>().interactable = true;
+            /*var rColors = rejectAll.GetComponent<Toggle>().colors;
+            rColors.normalColor = normal;
+            rejectAll.GetComponent<Toggle>().colors = rColors;*/
             doneBtn.gameObject.SetActive(false);
         }
     }
@@ -65,5 +77,63 @@ public class CookieManager : MonoBehaviour
         PlayerPrefs.SetInt(playerPrefsVarName,option);
         Debug.Log(playerPrefsVarName + " option selected: " + PlayerPrefs.GetInt(playerPrefsVarName));
         cookieOptions.SetActive(false);
+    }
+
+    private void Activate(int option)
+    {
+        if (option == 1)
+        {
+            //acceptAll.GetComponent<Toggle>().interactable = false;
+            /*var aColors = acceptAll.GetComponent<Toggle>().colors;
+            aColors.normalColor = notSelected;
+            acceptAll.GetComponent<Toggle>().colors = aColors;*/
+            acceptAll.GetComponent<Toggle>().isOn = false;
+            //strictlyNec.GetComponent<Toggle>().interactable = false;
+            /*var sColors = strictlyNec.GetComponent<Toggle>().colors;
+            sColors.normalColor = notSelected;
+            strictlyNec.GetComponent<Toggle>().colors = sColors;*/
+            strictlyNec.GetComponent<Toggle>().isOn = false;
+
+
+            doneBtn.gameObject.SetActive(true);
+            this.option = 1;
+        } 
+        else if (option == 2)
+        {
+            //acceptAll.GetComponent<Toggle>().interactable = false;
+            /*var aColors = acceptAll.GetComponent<Toggle>().colors;
+            aColors.normalColor = notSelected;
+            acceptAll.GetComponent<Toggle>().colors = aColors;*/
+            acceptAll.GetComponent<Toggle>().isOn = false;
+            //rejectAll.GetComponent<Toggle>().interactable = false;
+            /*var rColors = rejectAll.GetComponent<Toggle>().colors;
+            rColors.normalColor = notSelected;
+            rejectAll.GetComponent<Toggle>().colors = rColors;*/
+            rejectAll.GetComponent<Toggle>().isOn = false;
+
+            doneBtn.gameObject.SetActive(true);
+            this.option = 2;
+        }
+        else if (option == 3)
+        {
+            //strictlyNec.GetComponent<Toggle>().interactable = false;
+            /*var sColors = strictlyNec.GetComponent<Toggle>().colors;
+            sColors.normalColor = notSelected;
+            strictlyNec.GetComponent<Toggle>().colors = sColors;*/
+            strictlyNec.GetComponent<Toggle>().isOn = false;
+
+            //rejectAll.GetComponent<Toggle>().interactable = false;
+            /*var rColors = rejectAll.GetComponent<Toggle>().colors;
+            rColors.normalColor = notSelected;
+            rejectAll.GetComponent<Toggle>().colors = rColors;*/
+            rejectAll.GetComponent<Toggle>().isOn = false;
+
+            doneBtn.gameObject.SetActive(true);
+            this.option = 0;
+        }
+        else
+        {
+            Debug.Log("Cookie Selection Error");
+        }
     }
 }
